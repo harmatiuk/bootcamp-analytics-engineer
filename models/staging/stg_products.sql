2e98a6297dbe221502ff5b9544495e2e13864cf6
Lucas Harmatiuk <financeiro@dataleanbi.com>
2022-07-05 11:31:01 +0000

    create stg_products

---
+++ models/staging/stg_products.sql
@@ -0,0 +1,23 @@
+with source as (
+
+    select * from {{ from('products') }}
+
+)
+
+
+select
+    supplier_ids,
+    id,
+    product_code,
+    product_name,
+    description,
+    standard_cost,
+    list_price,
+    reorder_level,
+    target_level,
+    quantity_per_unit,
+    discontinued,
+    minimum_reorder_quantity,
+    category,
+    attachments
+from source

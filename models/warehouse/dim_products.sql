293c5bec7b18da00185c4ff240a47f01e7b27b8b
Lucas Harmatiuk <financeiro@dataleanbi.com>
2022-07-08 10:28:16 +0000

    change sql statement  dim_products and stg_products

--- models/warehouse/dim_products.sql
+++ models/warehouse/dim_products.sql
@@ -1,22 +1,40 @@
-with source as (
+with source_products as (
 
     select
-        id as product_id,
-        product_code,
-        product_name,
-        description,
-        supplier_ids as supplier_company,
-        standard_cost,
-        list_price,
-        reorder_level,
-        target_level,
-        quantity_per_unit,
-        discontinued,
-        minimum_reorder_quantity,
-        category,
-        attachments,
-        insertion_timestamp
+      *
     from {{ ref('stg_products') }}
+
+),
+
+
+source_suppliers as (
+
+    select
+        *
+    from {{ ref('stg_suppliers') }}
+),
+
+products as (
+
+       select
+            products.id as product_id,
+            products.product_code,
+            products.product_name,
+            products.description,
+            suppliers.company as supplier_company,
+            products.standard_cost,
+            products.list_price,
+            products.reorder_level,
+            products.target_level,
+            products.quantity_per_unit,
+            products.discontinued,
+            products.minimum_reorder_quantity,
+            products.category,
+            products.attachments,
+            products.insertion_timestamp
+        from source_products products
+        left join source_suppliers suppliers
+                on products.supplier_ids = suppliers.id
 
 ),
 
@@ -25,7 +43,7 @@ remove_duplicates as (
     select
         *,
         row_number() over(partition by product_id) as row_number
-    from source
+    from products
 )
 
 select

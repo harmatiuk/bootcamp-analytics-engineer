77d8ddaae9fdec9abf399d4733ddf8e8dfa48c09
Lucas Harmatiuk <financeiro@dataleanbi.com>
2022-07-11 12:34:50 +0000

    create fact_inventory

--- models/warehouse/fact_inventory.sql
+++ models/warehouse/fact_inventory.sql
@@ -1,6 +1,17 @@
 with source_invetory_transaction as (
 
-    select * from {{ ref('stg_inventory_transaction_types') }}
+    select * from {{ ref('stg_inventory_transactions') }}
 )
 
-select * from source_invetory_transaction
+select
+    id as inventory_id,
+    transaction_type,
+    transaction_created_date,
+    transaction_modified_date,
+    product_id,
+    quantity,
+    purchase_order_id,
+    customer_order_id,
+    comments,
+    insertion_timestamp
+from source_invetory_transaction
